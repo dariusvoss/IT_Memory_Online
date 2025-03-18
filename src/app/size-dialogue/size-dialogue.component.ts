dc1988fee5b6fd3a4f52e9738bedf6e124742096
darius <dariusvoss63@gmail.com>
2025-03-18 23:23:44 +0100

    creation of size_dialogue
    - creation of size_dialogue-component to enable the user to choose the amount of cards used in the game

---
+++ src/app/size-dialogue/size-dialogue.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-size-dialogue',
+  standalone: true,
+  imports: [],
+  templateUrl: './size-dialogue.component.html',
+  styleUrl: './size-dialogue.component.css'
+})
+export class SizeDialogueComponent {
+
+}
