816ac4f63e8450615370d684dc80770560cb4b72
darius <dariusvoss63@gmail.com>
2025-03-22 22:33:53 +0100

    addition of method 'openScoreboard()'
    - addition of method 'openScoreboard()' which closes the finish-dialog, opens the scoreboard and is called when the player clicks on the button 'Scoreboard'

--- src/app/dialog/board/finish-dialog/finish-dialog.component.ts
+++ src/app/dialog/board/finish-dialog/finish-dialog.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
-
+import { ScoreboardComponent } from '../../../scoreboard/scoreboard.component';
 
 @Component({
   selector: 'app-finish-dialog',
   imports: [CommonModule],
   templateUrl: './finish-dialog.component.html',
-  styleUrl: './finish-dialog.component.css'
+  styleUrls: ['./finish-dialog.component.css']
 })
 export class FinishDialogComponent {
   @Input() time: string = ''; // Zeit des Spiels
@@ -16,9 +16,14 @@ export class FinishDialogComponent {
   @Input() difficulty: string = ''; // Schwierigkeitsgrad
   @Input() message: string = ''; // Nachricht an den Spieler
 
-  constructor(public activeModal: NgbActiveModal) {}
+  constructor(public activeModal: NgbActiveModal, private modalService: NgbModal) {}
 
   closeDialog() {
     this.activeModal.close();
+  }
+
+  openScoreboard() {
+    this.closeDialog();
+    this.modalService.open(ScoreboardComponent, { size: 'lg', centered: true });
   }
 }
