ac5727afbec01502fe3a4a0646499ccafd4c1f1c
Dominik21-bit <dominik.schule21@gmail.com>
2025-03-30 12:25:38 +0200

    Bug fix on game-dialog
    when the player stop the "game" in the player vs time mode the bot is sometimes active when the player stop the game unfavorable

--- src/app/menu/size-dialogue/game-dialog/game-dialog.component.ts
+++ src/app/menu/size-dialogue/game-dialog/game-dialog.component.ts
@@ -58,11 +58,9 @@ export class GameDialogComponent implements OnInit, OnDestroy {
     if (this.timer.isTimerRunning) {
       this.timer.stopTimer();
       this.currentImage = '../assets/icons/Play.png';
-      this.gameService.isPlayerTurn = false; // damit der Spieler nicht weitere Karten aufdecken kann
     } else {
       this.timer.startTimer();
       this.currentImage = '../assets/icons/Stop.png';
-      this.gameService.isPlayerTurn = true;
     }
   }
 
